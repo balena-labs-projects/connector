e2e713b30b9b41db170779013a48b95607fc1713
Phil <phil@balena.io>
2020-08-06 10:42:55 +0100

    GH package investigation branch - NOT TO BE MERGED

--- download.sh
+++ download.sh
@@ -3,8 +3,10 @@
 outfile="/tmp/telegraf.tar.gz"
 download_base="https://dl.influxdata.com/telegraf/releases/"
 case $1 in
-   aarch64) package_file="telegraf-1.15.0~rc4_linux_arm64.tar.gz"
-       ;;
+    aarch64) package_file="telegraf-1.15.0~rc4_linux_arm64.tar.gz"
+        ;;
+    genericx86-64-ext) package_file="telegraf-1.15.0~rc4_linux_amd64.tar.gz"
+        ;;
     *) package_file="telegraf-1.15.0~rc4_linux_armhf.tar.gz"
 esac
 wget -O "${outfile}" "${download_base}${package_file}"
