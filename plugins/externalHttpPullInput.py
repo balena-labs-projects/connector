17c7b7d4f99dd0d1b4e69e3365f5d3bb4429ad95
Chris Crocker-White <chriscw@balena.io>
2020-08-16 13:49:41 +0100

    Add passthrough for ext. HTTP headers fixes #14
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- plugins/externalHttpPullInput.py
+++ plugins/externalHttpPullInput.py
@@ -21,6 +21,16 @@ def getConfigSection(url, name):
   data_format = "json"
   name_override = "{name}"
 """.format(url=url, name=name)
+
+    headers_found = False
+    headers = ""
+    for env_var,env_var_value in os.environ.items():
+      if env_var.startswith('EXTERNAL_HTTP_PULL_HEADER_'):
+        headers_found = True
+        headers = headers + "    " + env_var[26:] + " = \"" + env_var_value + "\"\n"
+
+    if headers_found == True:
+      output = output + "  [inputs.http.headers]\n"
+      output = output + headers
+
     return output
-
-
