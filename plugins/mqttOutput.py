61fd47bf856d592eb3f04534de9646d6ad720673
Phil Wilson <phil@balena.io>
2020-12-15 16:12:51 +0000

    Added topic prefix
    Change-type: patch
    Signed-off-by: Phil Wilson <phil@balena.io>

--- plugins/mqttOutput.py
+++ plugins/mqttOutput.py
@@ -13,19 +13,20 @@ def invoke(services):
 def getConfigSection(server, port):
     output = """
 [[outputs.mqtt]]
-  servers = "{url}:{port}"
+  servers = ["{url}:{port}"]
+  topic_prefix = "balena"
   data_format = "json"
-  qos =2
+  qos = 2
 """.format(url=server, port=port)
 
     username = os.environ.get('MQTT_OUTPUT_USERNAME')
     if(username is not None):
-        usernameSection = """  username = [{value}]\n""".format(value=username)
+        usernameSection = """  username = '{value}'\n""".format(value=username)
         output = output + usernameSection
     
     password = os.environ.get('MQTT_OUTPUT_PASSWORD')
     if(password is not None):
-        passwordSection = """  password = [{value}]\n""".format(value=password)
+        passwordSection = """  password = '{value}'\n""".format(value=password)
         output = output + passwordSection
 
     return output
