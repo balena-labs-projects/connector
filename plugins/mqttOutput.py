e10e419b0423dface57637251dc563792129fe70
Phil Wilson <phil@balena.io>
2020-12-15 14:53:59 +0000

    MQTT output plugin
    Change-type: patch
    Signed-off-by: Phil Wilson <phil@balena.io>

---
+++ plugins/mqttOutput.py
@@ -0,0 +1,33 @@
+import os
+
+SERVICE_NAME = "MQTT Push"
+
+def invoke(services):
+    server = os.environ.get('MQTT_OUTPUT_SERVER') or None
+    port = os.environ.get('MQTT_OUTPUT_PORT') or '1883'
+    if(server is None):
+        return None
+    print("Loading {name} plugin".format(name=SERVICE_NAME))
+    return getConfigSection(server, port)    
+
+def getConfigSection(server, port):
+    output = """
+[[outputs.mqtt]]
+  servers = "{url}:{port}"
+  data_format = "json"
+  qos =2
+""".format(url=server, port=port)
+
+    username = os.environ.get('MQTT_OUTPUT_USERNAME')
+    if(username is not None):
+        usernameSection = """  username = [{value}]\n""".format(value=username)
+        output = output + usernameSection
+    
+    password = os.environ.get('MQTT_OUTPUT_PASSWORD')
+    if(password is not None):
+        passwordSection = """  username = [{value}]\n""".format(value=password)
+        output = output + passwordSection
+
+    return output
+
+
